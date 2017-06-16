ca76a8958ec603a6075d7fe14df7cb337a9e72ed
Jack Howard <johncarverhoward@gmail.com>
2017-06-16 15:45:28 -0500

    work on db stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-u = User.create!(name: "Jack", email: "jack@email", hashed_password: "poo")
+u = User.create!(name: "Jack", email: "jack@email", password: "password")
 
 u.reviews.create!(headline: "great", body: "wonderful", imdb_id: 550)
